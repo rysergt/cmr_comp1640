aa48ddf0cd1304ae3f982973b5d1c19357d4c230
Tien Thanh <thanhvt1609@gmail.com>
2016-02-28 17:21:35 +0700

    scaffold Courses

---
+++ src/CMR/Models/Course.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CMR.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string code { get; set; }
+        public string name { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+    }
+}
